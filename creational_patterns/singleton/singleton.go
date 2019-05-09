4b85829a2e42d3082592b8d79510e0e06885d36e
driverzhang <631976053@qq.com>
2019-05-09 23:03:21 +0800

    单例模式详情添加实例

--- creational_patterns/singleton/singleton.go
+++ creational_patterns/singleton/singleton.go
@@ -8,6 +8,7 @@ type singleton struct {
 
 var instance *singleton
 
+// 懒汉模式-非并发
 func GetInstance() *singleton {
 	if instance == nil {
 		//instance = new(singleton)
@@ -26,13 +27,40 @@ func (s *singleton) GetCount() int {
 	return s.count
 }
 
+// 懒汉模式-并发-耗资源版
+var mutx sync.Mutex
+
+func GetInstance2() *singleton {
+	mutx.Lock()
+	defer mutx.Unlock()
+	if instance == nil {
+		instance = &singleton{}
+	}
+
+	return instance
+}
+
+// 懒汉模式-并发-优化资源版（双检查）
+func GetInstancConcurrent() *singleton {
+	if instance == nil {
+		// 这里可能多个线程处于此处
+		mutx.Lock()
+		if instance == nil {
+			instance = &singleton{}
+		}
+		mutx.Unlock()
+	}
+	return instance // 当一线程初始化后返回并赋值instance，则 instance 为 非nil
+}
+
 var once sync.Once
+
 // go 特有包单例模式高并发终极版推荐
-// 支持高并发单例模式
-func GetInstanceOnce() *singleton  {
+// 支持高并发单例模式（优雅）
+func GetInstanceOnce() *singleton {
 	once.Do(func() {
 		if instance == nil {
-			instance =  new(singleton)
+			instance = new(singleton)
 		}
 	})
 	return instance
