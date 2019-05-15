a0a8d4643e0d7cf45033e0589f257287121859a1
driverzhang <631976053@qq.com>
2019-05-15 21:26:28 +0800

    factory_meth

--- creational_patterns/factory_method/factory_method.go
+++ creational_patterns/factory_method/factory_method.go
@@ -1,1 +1,25 @@
 package factory_method
+
+type Data interface {
+	GetData(id string) string
+}
+
+func Get(name string) Data {
+	switch name {
+	case "logs":
+		return &Logs{}
+	case "simBase":
+		return &SimBase{}
+	default:
+		return nil
+	}
+}
+
+type Logs struct {
+}
+
+func (l Logs) GetData(id string) string { return "this is logs id:" + id }
+
+type SimBase struct{}
+
+func (s SimBase) GetData(id string) string { return "this is simbase ids:" + id }
--- creational_patterns/factory_method/factory_method_test.go
+++ creational_patterns/factory_method/factory_method_test.go
@@ -1,1 +1,17 @@
 package factory_method
+
+import "testing"
+
+func TestGet(t *testing.T) {
+	factory := Get("logs")
+	rsp := factory.GetData("123")
+	if rsp != "this is logs id:123" {
+		t.Error("error logs")
+	}
+
+	sim := Get("simBase")
+	rspSim := sim.GetData("321")
+	if rspSim != "this is simbase ids:321" {
+		t.Error("error simbase")
+	}
+}
